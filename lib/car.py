477cfe62ce34db744f29d345fe7914e2cc083644
lubnaa <lubnaamuhammed@gmail.com>
2025-08-14 21:38:53 +0300

    final commit

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,8 @@
 from vehicle import Vehicle
 
-class Car(Vehicle):
-    pass
+class Car(Vehicle): 
+    def go(self):   
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+my_car = Car(48, 4)
+print(my_car.go()) 
